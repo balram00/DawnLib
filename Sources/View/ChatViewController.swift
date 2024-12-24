86a74603a758f2cd2965365258962114840ab3c0
balram <balram.ak@bitcot.com>
2024-12-24 16:31:24 +0530

    test 3

--- Sources/View/ChatViewController.swift
+++ Sources/View/ChatViewController.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class ChatViewControllers: UIViewController {
+public class ChatViewControllers: UIViewController {
 
     // Outlets
     @IBOutlet weak var chatInputView: UIView!
