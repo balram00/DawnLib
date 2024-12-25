c50774f0f78a7127a936744f3c5c9e2eb679be27
balram <balram.ak@bitcot.com>
2024-12-25 11:27:28 +0530

    dsxdfgcewfr

--- Package.swift
+++ Package.swift
@@ -21,7 +21,7 @@ let package = Package(
             path: "Sources",
             resources: [
                 // Add resources like storyboards, XIBs, images, etc.
-                .process("Resources/Storyboards/Main.storyboard"),  // Storyboard
+                .process("Resources/Storyboards"),  // Storyboard
 //                .process("Resources/Images/logo.png"),              // Images
 //                .process("Resources/Fonts/CustomFont.ttf"),         // Fonts (if any)
 //                .process("Resources/XIBs/CustomView.xib")           // XIB files
