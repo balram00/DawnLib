289de6891e9fc05a59fbbc59884dc73e8c368890
balram <balram.ak@bitcot.com>
2024-12-25 15:48:45 +0530

    bsgdvbdf qdq

--- Package.swift
+++ Package.swift
@@ -21,7 +21,13 @@ let package = Package(
             path: "Sources",
             resources: [
                 // Add resources like storyboards, XIBs, images, etc.
-                .process("Resources/Storyboards/Main.storyboard")
+                .process("Resources/Storyboards/Main.storyboard"),
+                .process("Resources/Nibs/ChatHeaderCell.xib"),
+                .process("Resources/Nibs/ChatFooterCell.xib"),
+                .process("Resources/Nibs/AnswerTableViewCell.xib"),
+                .process("Resources/Nibs/QuestionTableViewCell.xib"),
+                .process("Resources/Nibs/FeedbackTableViewCell.xib"),
+                .process("Resources/Nibs/LoaderTableViewCell.xib")
 //                .process("Resources/Images/logo.png"),              // Images
 //                .process("Resources/Fonts/CustomFont.ttf"),         // Fonts (if any)
 //                .process("Resources/XIBs/CustomView.xib")           // XIB files
