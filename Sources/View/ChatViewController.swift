993cc09eb30d0ea4cc647ae3b0d9300d067f3c79
balram <balram.ak@bitcot.com>
2024-12-25 11:29:51 +0530

    dasfdgfnr

--- Sources/View/ChatViewController.swift
+++ Sources/View/ChatViewController.swift
@@ -80,19 +80,10 @@ public class ChatViewController: UIViewController {
        }
     
     public static func instantiate() -> ChatViewController {
-        // Ensure Bundle.module contains the storyboard
-        guard let _ = Bundle.module.path(forResource: "Main", ofType: "storyboardc") else {
-            fatalError("Main.storyboard not found in Bundle.module")
-        }
-
         let storyboard = UIStoryboard(name: "Main", bundle: Bundle.module)
-
-
-        // Instantiate ChatViewController from the storyboard
         guard let viewController = storyboard.instantiateViewController(withIdentifier: "ChatViewController") as? ChatViewController else {
-            fatalError("ChatViewController not found in Main.storyboard or identifier mismatch")
+            fatalError("ChatViewController not found in Main.storyboard")
         }
-
         return viewController
     }
 
